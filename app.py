1951c6b23d4d842892e059ad68b325d89fc75fa4
nerdlaunda <nerdlaunda@gmail.com>
2021-08-12 23:44:48 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+from flask import render_template
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    return 'Hello from hostname: ' + hostname + '\nHello from env.hostname: ' + os.environ['POD_NAME'] if "POD_NAME" in os.environ else "none" + '\nIP:' + local_ip + '\n'
+
+@app.route('/host')
+def host():
+
+    return ''
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="80")
+
+
+    
